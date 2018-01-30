2c9d25f576d259da5b6adc26e8ded47dbbebc220
chandanch <chandan.cec107@gmail.com>
2018-01-30 16:51:25 +0530

    add angular form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'app';
   private showSet = false;
+  private myform: FormGroup;
+
+  ngOnInit() {
+    this.myform = new FormGroup({
+      question1: new FormControl(),
+      question2: new FormControl(),
+      question3: new FormControl(),
+      question4: new FormControl(),
+      question5: new FormControl(),
+      question6: new FormControl(),
+    });
+  }
+
+  checkboxValue(selectedValue) {
+    console.log(selectedValue.source.value);
+    this.showSet = !this.showSet;
+  }
+
+
 }
